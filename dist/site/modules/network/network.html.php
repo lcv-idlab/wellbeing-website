203c29ad0970a53dec8a953d3b9a9bc9907c405f
Marco Lurati <marco.lurati@supsi.ch>
2017-06-07 18:10:52 +0200

    backup after publication

--- dist/site/modules/network/network.html.php
+++ dist/site/modules/network/network.html.php
@@ -7,10 +7,11 @@
     <div class="partnerlist">
     <?php foreach($module->partner()->toStructure() as $item): ?>
       <div class="partner">
-        <h3><a href="<?php echo $item->link->url() ?>" target="_blank"><?php echo $item->name()->html() ?></a></h3>
+        <h3><?php echo $item->name()->html() ?></h3>
       <div class="lowerpart">
         <div class="text">
           <?php echo $item->text()->kt() ?>
+          <a href="<?php echo $item->link->url() ?>" target="_blank" class="textlink"><?php echo $item->link->url() ?></a>
         </div>
           <?php if ($item->logo()->isNotEmpty()): ?>
             <div class="img">
